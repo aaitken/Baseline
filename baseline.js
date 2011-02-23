669accfb97298a77342b840572172799b243aeeb
Alex Aitken <aaitken@alexaitken.net>
2011-02-23 16:05:04 -0700

    Toggle Button write-in
    and code refactoring to keep all logic within the constructor and
    prototype code.

--- baseline.js
+++ baseline.js
@@ -3,23 +3,37 @@ BL.Rule=function(){
     this.canvas=document.createElement('canvas');
     this.docWidth=document.body.offsetWidth;
     this.docHeight=document.body.offsetHeight;
-    this.lineHeight=arguments[0].lineHeight;
+    this.lineHeight=null;//set with init
     //Declaration initializes and draws
     this.init();
-    this.draw();
 };
 BL.Rule.prototype={
     init:function(){
-        this.canvas.setAttribute('width',this.docWidth);
-        this.canvas.setAttribute('height',this.docHeight);
-        this.canvas.setAttribute('id','baselineCanvas');
-        this.canvas.setAttribute('style','position:absolute;top:0;z-index:7000');
-        document.body.appendChild(this.canvas);
+        var bodyClass=document.body.className,
+            //regex by http://txt2re.com/
+            re1='.*?',//Non-greedy match on filler
+            re2='(baseline-)',//Word 1
+            re3='(\\d)',// Any Single Digit 1
+            re4='(\\d)',// Any Single Digit 2
+            p=new RegExp(re1+re2+re3+re4,["i"]),
+            m=p.exec(bodyClass);
+        if(m!=null){
+            var match=m[1]+m[2]+m[3];
+            this.lineHeight=+(m[2]+m[3]);
+            this.canvas.setAttribute('width',this.docWidth);
+            this.canvas.setAttribute('height',this.docHeight);
+            this.canvas.setAttribute('id','baselineCanvas');
+            this.canvas.setAttribute('style','position:absolute;top:0;z-index:7000;visibility:visible');
+            document.body.appendChild(this.canvas);
+            this.draw();
+        }
     },
     draw:function(){
         var cnv=document.getElementById('baselineCanvas'),
-        ctx=cnv.getContext('2d'),
-        lineHeight=this.lineHeight;
+            ctx=cnv.getContext('2d'),
+            btn=document.createElement('button'),
+            lineHeight=this.lineHeight,
+            that=this;
         for(i=0;i<=this.docHeight;i+=lineHeight){
             ctx.strokeStyle='#ddd';
             ctx.beginPath();
@@ -27,20 +41,17 @@ BL.Rule.prototype={
             ctx.lineTo(this.docWidth,(i+lineHeight)-0.5);
             ctx.stroke();
         };
+        btn.innerHTML='Toggle Line Rules';
+        btn.setAttribute('style','position:absolute;z-index:8000;font-size:12px;top:10px;left:10px')
+        btn.addEventListener('click',function(){
+            (function(){
+                this.canvas.style.visibility==='visible'?
+                    this.canvas.style.visibility='hidden':
+                    this.canvas.style.visibility='visible';
+            }.apply(that));
+        });
+        document.body.appendChild(btn);
+
     }
 };
-
-(function(){
-    var bodyClass=document.body.className,
-        /*regex by http://txt2re.com/*/
-        re1='.*?',//Non-greedy match on filler
-        re2='(baseline-)',//Word 1
-        re3='(\\d)',// Any Single Digit 1
-        re4='(\\d)',// Any Single Digit 2
-        p=new RegExp(re1+re2+re3+re4,["i"]),
-        m=p.exec(bodyClass);
-    if(m!=null){
-        var match=m[1]+m[2]+m[3];
-        new BL.Rule({lineHeight:+(m[2]+m[3])});
-    }
-}());
+new BL.Rule();
